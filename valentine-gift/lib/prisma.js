152d22a2b40ea9c0a5f515eaf9438bee1b1a7755
Andrew Warren <ajwdragon@gmail.com>
2026-02-13 18:13:42 -0500

    fixed prisma issue with hosting idk

--- valentine-gift/lib/prisma.js
+++ valentine-gift/lib/prisma.js
@@ -1,22 +1,14 @@
 // lib/prisma.js
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from '@prisma/adapter-pg';
-import "dotenv/config"; // ensure env vars are loaded
 
-// const globalForPrisma = globalThis;
+const globalForPrisma = globalThis;
 
-const connectionString = process.env.DATABASE_URL;
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
-export {prisma};
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ["error"],
+  });
 
-// export const prisma =
-//   globalForPrisma.prisma ||
-//   new PrismaClient({
-//     log: ["query"], // optional
-//     // adapter: "postgresql", // <--- REQUIRED in Prisma 7 for local Postgres
-//   });
-
-// if (process.env.NODE_ENV !== "production") {
-//   globalForPrisma.prisma = prisma;
-// }
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
